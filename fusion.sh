86bafbc2385c544dc2b2384e92bf7d26f0bcb494
adam-ff <85935763+adam-ff@users.noreply.github.com>
2025-06-19 21:55:56 +0100

    Create fusion.sh

---
+++ fusion.sh
@@ -0,0 +1,16 @@
+apt install git-all curl ffmpeg -y
+curl -LO https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/miniconda3
+source $HOME/miniconda3/bin/activate
+conda init --all
+source "$HOME/.bashrc"
+conda create --name facefusion python=3.12 pip=25.0
+source $HOME/miniconda3/bin/activate facefusion
+conda install conda-forge::cuda-runtime=12.8.1 conda-forge::cudnn=9.8.0.87 -y  
+git clone https://github.com/facefusion/facefusion 
+cd facefusion
+python install.py --onnxruntime cuda
+conda deactivate
+source $HOME/miniconda3/bin/activate facefusion
+python facefusion.py run
+wget $URL -O /workspace/facefusion/facefusion/$FILE
